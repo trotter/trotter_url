4177dcc24becdef3a5b044342f4a4e1f2949ed14
Trotter Cashion <cashion@gmail.com>
2009-05-02 17:48:43 -0400

    Parse relative urls

--- test/trotter_url_spec.js
+++ test/trotter_url_spec.js
@@ -4,9 +4,9 @@ Screw.Unit(function () {
     describe("Trotter.Url", function () {
         var url, input;
 
-        describe("Parsing a Url", function () {
+        describe("Parsing an absolute URL", function () {
             before(function () {
-                input = "http://localhost:3000/the/whole/path.js?firstname=bob&lastname=jones"
+                input = "http://trotter:test@localhost:3000/the/whole/path.js?firstname=bob&lastname=jones#theFrag"
                 url = new Trotter.Url(input);
             });
 
@@ -22,6 +22,10 @@ Screw.Unit(function () {
                 expect(url.host).to(equal, "localhost");
             });
 
+            it("should have userInfo", function () {
+                expect(url.userInfo).to(equal, "trotter:test");
+            });
+
             it("should have a port", function () {
                 expect(url.port).to(equal, 3000);
             });
@@ -32,6 +36,45 @@ Screw.Unit(function () {
 
             it("should have a query string", function () {
                 expect(url.queryString).to(equal, "firstname=bob&lastname=jones");
+            });
+
+            it("should have a fragment", function () {
+                expect(url.fragment).to(equal, "theFrag");
+            });
+        });
+
+        describe("Parsing a relative URL", function () {
+            before(function () {
+                input = "//trotter:test@localhost:3000/the/whole/path.js?firstname=bob&lastname=jones#theFrag"
+                url = new Trotter.Url(input);
+            });
+
+            it("should convert to a string", function () {
+                expect(url.toString()).to(equal, input);
+            });
+
+            it("should have a host", function () {
+                expect(url.host).to(equal, "localhost");
+            });
+
+            it("should have userInfo", function () {
+                expect(url.userInfo).to(equal, "trotter:test");
+            });
+
+            it("should have a port", function () {
+                expect(url.port).to(equal, 3000);
+            });
+
+            it("should have a path", function () {
+                expect(url.path).to(equal, "/the/whole/path.js");
+            });
+
+            it("should have a query string", function () {
+                expect(url.queryString).to(equal, "firstname=bob&lastname=jones");
+            });
+
+            it("should have a fragment", function () {
+                expect(url.fragment).to(equal, "theFrag");
             });
         });
     });
